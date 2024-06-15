0c4c0ff5eee3cfa2091b9e1ba19e0be9f050dfbe
mengx3 <156154923+mengx3@users.noreply.github.com>
2024-06-14 22:31:07 -0400

    Create FoodController.java

---
+++ FoodController.java
@@ -0,0 +1,13 @@
+package com.food.foodweb.controller;
+
+import com.food.foodweb.model.Food;
+import com.food.foodweb.service.FoodService;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
