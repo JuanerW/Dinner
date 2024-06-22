5c6b7356e9066173f703a5b1e8f5c2c99427e418
huangl9 <huangl9@rpi.edu>
2024-06-22 17:05:36 -0400

    Thomas Huang commit 2

---
+++ UserController.java
@@ -0,0 +1,40 @@
+package com.food.foodweb.controller;
+
+import com.food.foodweb.model.Food;
+import com.food.foodweb.model.User;
+import com.food.foodweb.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+public class UserController {
+    @GetMapping("/edituser")
+    public String toEdit(String username, Model model) {
+        User user = userService.findUserByName(username);
+        model.addAttribute("u", user);
+        return "usercenter";
+    }
+
+    @PostMapping("/edituser")
+    public String doEdit(User user, Model model) {
+        boolean ret = userService.updateuser(user);
+        System.out.println(ret);
+        if (ret) {
+            return "redirect:usersy";
+        } else {
+            return "usercenter";
+        }
+    }
+
+    @GetMapping("/UserManageSy")
+    public String CommentManageSy(Model model) {
+        List<User> users = userService.findAllUser();
+        model.addAttribute("users", users);
+        return "usermanagesy";
+    }
+}
