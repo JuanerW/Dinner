94594af417eacd77da39786748943cf80a12254d
SamanthaQiu <133875633+SamanthaQiu@users.noreply.github.com>
2024-06-22 20:24:37 -0400

    Update FoodInfoController.java
    add public class FoodInfoController

--- FoodInfoController.java
+++ FoodInfoController.java
@@ -13,3 +13,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
+
+@Controller
+public class FoodInfoController {
+    public FoodInfoService foodinfoService;
+
+    @Autowired
+    public void setFoodInfoService(FoodInfoService foodinfoService) {
+        this.foodinfoService = foodinfoService;
+    }
+
+    @GetMapping("/findinfo")
+    public String findFoodinfoByName(String name, Model model) {
+        Food food = foodinfoService.findFoodByName(name);
+        model.addAttribute("food", food);
+        return "make";
+    }
+}
