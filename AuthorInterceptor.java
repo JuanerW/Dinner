da141aa34fa26f892b47efa45c3b3e91a6ace280
mengx3 <156154923+mengx3@users.noreply.github.com>
2024-06-28 14:45:08 -0400

    Update AuthorInterceptor.java

--- AuthorInterceptor.java
+++ AuthorInterceptor.java
@@ -8,4 +8,19 @@ import javax.servlet.http.HttpServletResponse;
 
 public class AuthorInterceptor extends HandlerInterceptorAdapter {
     @Override
-    
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(request.getRequestURI().endsWith("/register")||request.getRequestURI().endsWith("/adminlogin")||request.getRequestURI().endsWith("/login")||request.getRequestURI().endsWith("/sy")||request.getRequestURI().endsWith(".css")||request.getRequestURI().endsWith(".js")||request.getRequestURI().endsWith(".png")||request.getRequestURI().endsWith(".jpg")||request.getRequestURI().endsWith(".gif")){
+            return true;
+        }
+        User user= (User) request.getSession().getAttribute("user");
+        Admin admin= (Admin) request.getSession().getAttribute("admin");
+        if(user==null){
+            if (admin==null)
+            { response.sendRedirect("login");
+                return false;}
+            else {return true;}
+        }
+        else
+        { return true;}
+    }
+}
