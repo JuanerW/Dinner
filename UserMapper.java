35b2bf1293db793547561e30cb035c1dee8ec26c
huangl9 <huangl9@rpi.edu>
2024-06-28 14:54:16 -0400

    Thomas Huang commit 5

--- UserMapper.java
+++ UserMapper.java
@@ -1,4 +1,30 @@
-package com.food.foodweb.controller;
+package com.food.foodweb.mapper;
 
-public class UserMapper {
+import com.food.foodweb.model.User;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+
+    @Select("SELECT * FROM tb_user WHERE username=#{username} and password=#{password}")
+    User find(User user);
+
+    @Insert("insert into tb_user values(#{username},#{password},#{sex},#{age},#{weight},#{height})")
+    boolean addUser(User user);
+
+    @Update("update tb_user set username=#{username},password=#{password},sex=#{sex},age=#{age},weight=#{weight},height=#{height} where username=#{username}")
+    boolean updateuser(User user);
+
+    @Select("select * from tb_user where username=#{username}")
+    User findbyname(String username);
+
+    @Select("select * from tb_user")
+    List<User> findAllUser();
+
+    @Delete("delete from tb_user where username=#{name}")
+    boolean delUserByName(String name);
 }
