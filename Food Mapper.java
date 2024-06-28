608687fcb9814cbbb536ba50005e3946ddd7e0c5
JuanerW <wangzhiyuanstan@outlook.com>
2024-06-28 14:52:18 -0400

    Update Food Mapper.java
    three more update in the Mapper

--- Food Mapper.java
+++ Food Mapper.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @Mapper
 public interface FoodMapper {
+
     @Select("select * from tb_food where name=#{name}")
     Food findByName(String name);
 
@@ -18,4 +19,13 @@ public interface FoodMapper {
 
     @Insert("insert into tb_food values(#{name},#{img},#{time},#{type},#{pop},#{way},#{culture},#{step})")
     boolean add(Food food);
+
+    @Delete("delete from tb_food where name=#{name}")
+    boolean del(String name);
+
+    @Update("update tb_food set name=#{name},img=#{img},time=#{time},type=#{type},pop=#{pop},way=#{way} where name=#{name}")
+    boolean edit(Food food);
+
+    @Update("update tb_food set name=#{name},img=#{img},culture=#{culture},step=#{step} where name=#{name}")
+    boolean editinfo(Food food);
 }
