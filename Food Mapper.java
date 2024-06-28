0be5531ed0e0555aec20fc0ca30b89bad910c704
JuanerW <wangzhiyuanstan@outlook.com>
2024-06-28 14:51:27 -0400

    Create Food Mapper.java
    Mapper file

---
+++ Food Mapper.java
@@ -0,0 +1,11 @@
+package com.food.foodweb.mapper;
+
+import com.food.foodweb.model.Food;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface FoodMapper {
+
+}
