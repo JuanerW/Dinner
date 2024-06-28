b783488f8803a3bf2f8d40a4f01ac83bff99b86b
JuanerW <wangzhiyuanstan@outlook.com>
2024-06-28 14:51:55 -0400

    Update Food Mapper.java
    four functions of select in the FoodMapper

--- Food Mapper.java
+++ Food Mapper.java
@@ -7,5 +7,15 @@ import java.util.List;
 
 @Mapper
 public interface FoodMapper {
+    @Select("select * from tb_food where name=#{name}")
+    Food findByName(String name);
 
+    @Select("select * from tb_food where name like concat(\"%\",#{name},\"%\")")
+    List<Food>vagueFindByName(String name);
+
+    @Select("select*from tb_food")
+    List<Food> findAll();
+
+    @Insert("insert into tb_food values(#{name},#{img},#{time},#{type},#{pop},#{way},#{culture},#{step})")
+    boolean add(Food food);
 }
