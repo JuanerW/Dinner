d567ed8e775a212b916c7e0183c8e28207c9b9fd
SamanthaQiu <133875633+SamanthaQiu@users.noreply.github.com>
2024-06-29 18:56:17 -0400

    Create FoodInfoMapper.java

---
+++ FoodInfoMapper.java
@@ -0,0 +1,16 @@
+package com.food.foodweb.mapper;
+
+import com.food.foodweb.model.Comment;
+import com.food.foodweb.model.Food;
+import com.food.foodweb.model.OrderedFood;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface FoodInfoMapper {
+
+}
