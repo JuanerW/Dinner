6a024bcaafde53a65351df4acc7b6a4333aebe4a
SamanthaQiu <133875633+SamanthaQiu@users.noreply.github.com>
2024-06-29 18:57:14 -0400

    Update FoodInfoMapper.java

--- FoodInfoMapper.java
+++ FoodInfoMapper.java
@@ -12,5 +12,33 @@ import java.util.List;
 
 @Mapper
 public interface FoodInfoMapper {
+    @Select("select*from tb_food where name=#{name}")
+    Food findfood(String name);
 
+    @Insert("insert into tb_orderedfood values(#{userName},#{foodName},#{date})")
+    boolean insertFoodByName(String userName, String foodName, String date);
+
+    @Select("select * from tb_orderedfood where user_name=#{name}")
+    List<OrderedFood> findCollectInfoByName(String name);
+
+    @Select("select * from tb_orderedfood where food_name=#{foodName}")
+    OrderedFood findCollectInfoByFoodName(String foodName);
+
+    @Delete("delete from tb_orderedfood where food_name=#{food_name} and user_name=#{user_name}")
+    boolean delcollect(String food_name, String user_name);
+
+    @Select("select * from tb_food")
+    List<Food> findAllinfo();
+
+    @Select("select*from tb_comment where username=#{name}")
+    List<Comment> findAllCommentsByName(String name);
+
+    @Insert("insert into tb_comment values(#{name},#{comment},#{date})")
+    boolean insertComment(String name, String comment, String date);
+
+    @Delete("delete from tb_comment where comment=#{comment}")
+    boolean commentDel(String comment);
+
+    @Select("select*from tb_comment")
+    List<Comment> findAllComments();
 }
