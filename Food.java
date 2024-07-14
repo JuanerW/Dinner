027035956c451bf35e48d82523d5d739e8431757
mengx3 <156154923+mengx3@users.noreply.github.com>
2024-07-13 21:28:08 -0400

    Create Food.java

---
+++ Food.java
@@ -0,0 +1,18 @@
+package com.food.foodweb.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Food {
+    private String name;
+    private String img;
+    private String way;
+    private String time;
+    private String type;
+    private String pop;
+    private String culture;
+    private String step;
+}
