cf4d5f77c895791e52e461ee80ddc6cb572e490b
SamanthaQiu <133875633+SamanthaQiu@users.noreply.github.com>
2024-07-13 23:48:56 -0400

    Create FoodInfoServiceImpl.java

---
+++ FoodInfoServiceImpl.java
@@ -0,0 +1,13 @@
+package com.food.foodweb.service.iml;
+
+import com.food.foodweb.mapper.FoodInfoMapper;
+import com.food.foodweb.model.Comment;
+import com.food.foodweb.model.Food;
+import com.food.foodweb.model.OrderedFood;
+import com.food.foodweb.service.FoodInfoService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
