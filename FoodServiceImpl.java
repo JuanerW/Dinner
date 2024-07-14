b03d81d0cb4609a8a9c246535354e54dd42e23d3
mengx3 <156154923+mengx3@users.noreply.github.com>
2024-07-13 21:28:52 -0400

    Create FoodServiceImpl.java

---
+++ FoodServiceImpl.java
@@ -0,0 +1,11 @@
+package com.food.foodweb.service.iml;
+
+import com.food.foodweb.mapper.FoodMapper;
+import com.food.foodweb.model.Food;
+import com.food.foodweb.service.FoodService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
