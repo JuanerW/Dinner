039dc0ebd19dda84f213f297084fa90dfc0fb41d
阿娟 <wangzhiyuanstan@outlook.com>
2024-07-20 00:14:32 -0400

    Update FoodServiceImpl.java
    123

--- FoodServiceImpl.java
+++ FoodServiceImpl.java
@@ -1,11 +1,50 @@
 package com.food.foodweb.service.iml;
 
 import com.food.foodweb.mapper.FoodMapper;
-import com.food.foodweb.model.Food;
-import com.food.foodweb.service.FoodService;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
+import com.food.foodweb.mapper.UserMapper;
+import com.food.foodweb.model.User;
+import com.food.foodweb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    public UserMapper userMapper;
+
+    @Autowired
+    public void setUserDao(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public User findUser(User user) {
+        return userMapper.find(user);
+    }
+
+    @Override
+    public boolean addUser(User user) {
+        return userMapper.addUser(user);
+    }
+
+    @Override
+    public boolean updateuser(User user) {
+        return userMapper.updateuser(user);
+    }
+
+    @Override
+    public User findUserByName(String username) {
+        return userMapper.findbyname(username);
+    }
+
+    @Override
+    public List<User> findAllUser() {
+        return userMapper.findAllUser();
+    }
+
+    @Override
+    public boolean delUserByName(String name) {
+        return userMapper.delUserByName(name);
+    }
+}
