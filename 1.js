c868ffb4c51b53bce448f6a892ae9040ed460161
SamanthaQiu <133875633+SamanthaQiu@users.noreply.github.com>
2024-07-19 23:55:24 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -17,3 +17,23 @@ $(".DH .daohang ul li").hover(
     },function(){
         $(this).css("background-color","#FFFFFF")
     });
+    /************* More ingredients change color *************/
+    $(".CT .content .content_c2 .food_material1 .more a").hover(function(){
+        $(this).css({
+            "color":"red",
+            "text-decoration":"underline"
+        });
+    },function(){
+        $(this).css({
+            "color":"#3B3B3B",
+            "text-decoration":"none"
+        });
+    });
+
+    $(".food_material2 .food_up .food_u").mouseover(function(){
+        $(this).toggleClass("food_p1");//Checks for the class specified in each element. Add the class if it does not exist, or remove it if it is already set. This is called the toggle effect
+        $(this).find("a").toggleClass("food_p2");
+        var index=$(this).index();
+        $(".food_d").eq(index).css("display","block");
+        $(".food_d").eq(index).siblings().css("display","none");
+    });
