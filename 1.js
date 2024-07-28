03f0d06ef7af458aed0f7061b3348112fcedc566
mengx3 <156154923+mengx3@users.noreply.github.com>
2024-07-27 20:57:58 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -117,3 +117,19 @@ function dakemode() {
         sign=false;
     }
 }
+
+$(function () {
+    var t = null;
+    t = setTimeout(time, 1000);//開始运行
+    function time() {
+        clearTimeout(t);//清除定时器
+        dt = new Date();
+        var year = dt.getFullYear();
+        var month = dt.getMonth() + 1;//(0-11,0代表1月)
+        var date = dt.getDate();//获取天
+        var num = dt.getDay();//获取星期
+        var weekday = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
+        var hour = dt.getHours();//获取时
+        var minute = dt.getMinutes();//获取分
+        var second = dt.getSeconds();//获取秒
+}
