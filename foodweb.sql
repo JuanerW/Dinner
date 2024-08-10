f32eff200f81ea939bd72b06d5019f73ade575ca
阿娟 <wangzhiyuanstan@outlook.com>
2024-08-09 21:50:33 -0400

    Create foodweb.sql
    new sql database

---
+++ foodweb.sql
@@ -0,0 +1,40 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tb_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_admin`;
+CREATE TABLE `tb_admin`  (
+  `adminname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `adminpassword` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_admin
+-- ----------------------------
+INSERT INTO `tb_admin` VALUES ('admin', 'admin');
+
+-- ----------------------------
+-- Table structure for tb_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_comment`;
+CREATE TABLE `tb_comment`  (
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `date` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_comment
+-- ----------------------------
+INSERT INTO `tb_comment` VALUES ('Zhou Jie', 'Yangzhou Fried Rice', '2024-05-20 22:16:58');
+INSERT INTO `tb_comment` VALUES ('Xiao Zhang', 'It should be Beggar’s Chicken~', '2024-05-20 22:17:13');
+INSERT INTO `tb_comment` VALUES ('Xiao Zhang', 'Everything is delicious!!!', '2024-05-20 22:17:26');
+
+-- ----------------------------
+-- Table structure for tb_food
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_food`;
+CREATE TABLE `tb_food`  (
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
