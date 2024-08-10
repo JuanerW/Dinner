6451e6ac154b21dcf6dfc13ed9f5b16118cde069
SamanthaQiu <133875633+SamanthaQiu@users.noreply.github.com>
2024-08-10 18:44:12 -0400

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,16 @@
+package com.food.foodweb;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import lombok.NonNull;
+
+import javax.naming.Context;
+import javax.swing.text.Document;
+import java.util.List;
